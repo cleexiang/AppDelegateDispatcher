3f0b529c408412f624cea70d9efe1c16aef5c52a
cleexiang <cleexiang@126.com>
2018-01-17 11:15:16 +0800

    Initial Commit

---
+++ AppDelegateDispatcher/AppDelegateDispatcher.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegateDispatcher.h
+//  AppDelegateDispatcher
+//
+//  Created by 李响 on 2018/1/17.
+//  Copyright © 2018年 Lixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppDelegateDispatcher.
+FOUNDATION_EXPORT double AppDelegateDispatcherVersionNumber;
+
+//! Project version string for AppDelegateDispatcher.
+FOUNDATION_EXPORT const unsigned char AppDelegateDispatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppDelegateDispatcher/PublicHeader.h>
+
+
